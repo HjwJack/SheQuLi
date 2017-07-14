10d39f0db511aab9ad6c3bda77ad5725755704f5
HuJiangWei <18910207853@163.com>
2017-07-14 16:10:31 +0800

    First commit

---
+++ app/src/main/java/app/shequli/yjlm/com/shequli/MainActivity.java
@@ -0,0 +1,13 @@
+package app.shequli.yjlm.com.shequli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
